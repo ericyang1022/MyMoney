2b9b71d5626aca9e8207ace5f7a3e03f764a34db
Shaun Abram <shaunabram@gmail.com>
2012-10-13 23:38:57 -0700

    Fix for date format issue in transactions

--- src/main/java/com/mymoney/editor/GlobalBindingInitializer.java
+++ src/main/java/com/mymoney/editor/GlobalBindingInitializer.java
@@ -1,15 +1,23 @@
 package com.mymoney.editor;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.support.WebBindingInitializer;
 import org.springframework.web.context.request.WebRequest;
 
 import com.domainlanguage.money.Money;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class GlobalBindingInitializer implements WebBindingInitializer {
 
     public void initBinder(WebDataBinder binder, WebRequest request) {
         binder.registerCustomEditor(Money.class, new MoneyEditor());
+
+        CustomDateEditor editor = new CustomDateEditor(new SimpleDateFormat("MM/dd/yyyy"), true);
+        binder.registerCustomEditor(Date.class, editor);
+
     }
 
 }
